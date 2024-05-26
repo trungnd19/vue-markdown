7941c0864b6aaa1c49059dba8e8c29153c2976f0
trungnguyenduc0101 <trungnguyenduc0101@gmail.com>
2024-05-27 00:46:23 +0700

    feat: add luxon lib

---
+++ src/components/Timeline.vue
@@ -0,0 +1,23 @@
+<template>
+    <nav class="is-primary panel">
+        <span class="panel-tabs">
+            <a v-for="period of periods" :key="period" @click="selectPeriod(period)" 
+            :class="{ 'is-active': period === selectedPeriod}">{{ period }}</a>
+        </span>
+    </nav>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const periods = ["Today", "This week", "This month"] as const
+type Period = typeof periods[number]
+
+const selectedPeriod = ref<Period>("Today")
+
+function selectPeriod(period: Period) {
+    selectedPeriod.value = period
+}
+</script>
+
+<style scoped></style>
