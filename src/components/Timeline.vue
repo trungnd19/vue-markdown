f9b7503161647f22f8e9aee9a6bbeb5346427243
trungnguyenduc0101 <trungnguyenduc0101@gmail.com>
2024-06-03 22:33:44 +0700

    feat: finish section 1

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,23 +1,57 @@
 <template>
-    <nav class="is-primary panel">
-        <span class="panel-tabs">
-            <a v-for="period of periods" :key="period" @click="selectPeriod(period)" 
-            :class="{ 'is-active': period === selectedPeriod}">{{ period }}</a>
-        </span>
-    </nav>
+  <nav class="is-primary panel">
+    <span class="panel-tabs">
+      <a
+        v-for="period of periods"
+        :key="period"
+        @click="selectPeriod(period)"
+        :class="{ 'is-active': period === selectedPeriod }"
+        >{{ period }}</a
+      >
+    </span>
+    <TimelineItem
+      v-for="postItem of posts"
+      :post="postItem"
+      :key="postItem.id"
+    ></TimelineItem>
+  </nav>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, ref } from "vue";
+import { TimelinePost, thisMonth, thisWeek, today } from "../posts";
+import { DateTime } from "luxon";
+import TimelineItem from "./TimelineItem.vue";
 
-const periods = ["Today", "This week", "This month"] as const
-type Period = typeof periods[number]
+const periods = ["Today", "This week", "This month"] as const;
+type Period = (typeof periods)[number];
 
-const selectedPeriod = ref<Period>("Today")
+const selectedPeriod = ref<Period>("Today");
 
 function selectPeriod(period: Period) {
-    selectedPeriod.value = period
+  selectedPeriod.value = period;
 }
+
+const posts = computed<TimelinePost[]>(() => {
+  return [today, thisWeek, thisMonth]
+    .map((post) => {
+      return {
+        ...post,
+        created: DateTime.fromISO(post.created),
+      };
+    })
+    .filter((post) => {
+      if (selectedPeriod.value === "Today") {
+        return post.created >= DateTime.now().minus({ day: 1 });
+      }
+
+      if (selectedPeriod.value === "This week") {
+        return post.created >= DateTime.now().minus({ week: 1 });
+      }
+
+      return post;
+    });
+});
 </script>
 
 <style scoped></style>
---
+++ src/components/TimelineItem.vue
@@ -0,0 +1,18 @@
+<template>
+    <a class="panel-block is-flex is-flex-direction-column is-align-items-flex-start">
+        <a>{{ post.title }}</a>
+        <div>{{ post.created.toFormat("d MMM") }}</div>
+    </a>
+</template>
+
+<script setup lang="ts">
+import { TimelinePost } from '../posts';
+
+defineProps<{
+    post: TimelinePost
+}>()
+
+
+</script>
+
+<style scoped></style>
